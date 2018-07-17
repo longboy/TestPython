8108145e5f5565146d4afc968d69e1bca1c10686
ywj <ywj_486@163.com>
2018-07-17 18:42:42 +0800

    简单调用js

---
+++ seleniumtest/jstest.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(6)
+driver.get("https://www.baidu.com")
+time.sleep(1)
+# 弹框
+# driver.execute_script("window.alert('这是一个alert弹框。');")
+driver.get("https://tieba.baidu.com/index.html")
+time.sleep(1)
+
+target_elem = driver.find_element_by_link_text("人文自然")
+driver.execute_script("return arguments[0].scrollIntoView();", target_elem)

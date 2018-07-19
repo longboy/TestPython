69f55938ac5853a245be36b7b0c75a748bba72a9
ywj <ywj_486@163.com>
2018-07-19 17:29:39 +0800

    摘取当前页图片或元素信息

--- seleniumtest/imagetest.py
+++ seleniumtest/imagetest.py
@@ -8,8 +8,11 @@ driver.implicitly_wait(3)
 driver.get("http://news.baidu.com")
 
 time.sleep(3)
-
-for image in driver.find_elements_by_tag_name("img"):
-    print(image.size)
+# 图片信息
+# for image in driver.find_elements_by_tag_name("img"):
+#     print(image.size)
+# href元素
+for link in driver.find_elements_by_xpath("//*[@href]"):
+    print(link.get_attribute('href'))
 
 driver.quit()
